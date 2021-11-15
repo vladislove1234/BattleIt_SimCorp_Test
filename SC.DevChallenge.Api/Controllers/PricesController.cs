aa5db99c48c7651bffd7b5cf26d7b38e7cc20bfc
vladislove1234 <40027853+vladislove1234@users.noreply.github.com>
2021-11-15 15:41:25 +0200

    fix

--- SC.DevChallenge.Api/Controllers/PricesController.cs
+++ SC.DevChallenge.Api/Controllers/PricesController.cs
@@ -18,11 +18,13 @@ namespace SC.DevChallenge.Api.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult Average([FromQuery] string portfolio, [FromQuery] string owner, [FromQuery] string instrument, [FromQuery] DateTime datetime)//
         {
+            if (string.IsNullOrEmpty(portfolio) || string.IsNullOrEmpty(owner) || string.IsNullOrEmpty(instrument) || datetime == DateTime.MinValue)
+                return StatusCode(404);
             var ret = PriceManager.GetResponse(portfolio, owner, instrument, datetime);
             if (ret != null)
                 return Content(ret.ToString());
             else
-                return BadRequest();
+                return StatusCode(404);
         }
         [HttpGet("avarage_for_period")]
         public ActionResult AverageForPeriod([FromQuery] string portfolio, [FromQuery] string owner, [FromQuery] string instrument, [FromQuery] int period)//
